f95c4116802c5477b53b954332fbb60bea91aeed
Marklar <dmitri.bilyk@gmail.com>
2017-07-05 20:08:37 -0700

    recursive method, not compiling?

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -9,25 +9,25 @@ namespace Test1
 {
     class Program
     {
+        static int RecursiveMethod(int value, ref int counter)
+        {
+            counter++;
+            if (value >= 10)
+            {
+                return value;   
+            }
+            RecursiveMethod(value + 1,ref counter);
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            string path = @"C:\Dropbox\Text\arcadia show 2017 schedule.txt";
-            string ArcadiaFile = File.ReadAllText(@"C:\Dropbox\Text\arcadia show 2017 schedule.txt");
-            if (ArcadiaFile.Contains("!"))
-            {
-                ArcadiaFile = ArcadiaFile.Replace("!", "marklar!!!!!!!!!!! hahahahah i am writing to a file ya mafkas!");
 
-                File.WriteAllText(path, ArcadiaFile);
-                Console.WriteLine(ArcadiaFile);
-                string stuff = "test1";
-                
-
-            }
-            else
-            {
-                Console.Write("yabbish, theres nothin.");
-            }
-            
+            int counter = 0;
+            int total = RecursiveMethod(1,ref counter);
+            Console.WriteLine(total);
             Console.ReadLine();
 
         }
